acbf816935ec62fe53fd6a942a1f42bf19c0e884
moh1med <mohamedch047@gmail.com>
2023-08-26 13:54:36 +0100

    Completed assignment

---
+++ index.js
@@ -0,0 +1,63 @@
+
+function add() {
+    return (number += 3);
+  }
+  
+  
+  
+  function subtract(){
+        return (number -= 3);
+  
+  }
+  function multiply(){
+        return (number *= 3);
+  
+  }
+  
+  function divide() {
+    return (number /= 3);
+   
+  }
+  
+  // (a,b)
+  
+  function add(a, b) {
+    return (a+b);
+  }
+  
+  
+  
+  function subtract(a ,b){
+        return (a - b);
+  
+  }
+  function multiply(a ,b){
+        return (a * b);
+  
+  }
+  
+  function divide(a ,b) {
+    return (a / b);
+  }
+  
+  function increment(n){
+      return (++n);
+  }
+  
+  function decrement(n){
+      return (--n);
+  }
+  
+  function makeInt(n) {
+     
+      const n2 = parseInt(n, 10);
+  
+          return n2;
+  }
+  
+  function preserveDecimal(n) {
+      const n3 = parseFloat(n);
+      return n3;
+  
+  
+  }
